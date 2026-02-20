059753cdd76429454bbb6c82ed99ed9c7262c568
dhruv.dhruv23@st.niituniversity.in <dhruv.dhruv23@st.niituniversity.in>
2026-02-20 12:44:04 +0530

    implement evaluation using eval()

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ buttons.forEach(button => {
         }
     });
 });
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+
+        if (value === "C") {
+            display.value = "";
+        } 
+        else if (value === "=") {
+            try {
+                display.value = eval(display.value);
+            } catch {
+                display.value = "Error";
+            }
+        } 
+        else {
+            display.value += value;
+        }
+    });
+});
